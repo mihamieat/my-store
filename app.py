a714a37cae52500c9f7c786a62562606d2b564ed
Mihamina Rakotovazaha <mihamina.rakotovazaha@digeiz.com>
2023-06-12 23:03:49 +0200

    refactor: handle error on creating items

--- app.py
+++ app.py
@@ -51,11 +51,26 @@ def get_items_in_store(item_id):
 @app.post("/item")
 def create_item():
     """Creates a new item in a specified store."""
-    req = request.get_json()
-    if req["store_id"] not in stores:
-        return {"message": "Store not found."}, 404
+    item_data = request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', \
+and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if (
+            item_data["name"] == item["name"]
+            and item_data["store_id"] == item["store_id"]
+        ):
+            abort(400, message="Item already exists.")
+
     item_id = uuid4().hex
-    item = {**req, "id": item_id}
+    item = {**item_data, "id": item_id}
     items[item_id] = item
 
-    return {"message": "Item created.", "item": item}, 201
+    return item
