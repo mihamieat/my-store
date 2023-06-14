c74cd2989a86c90dac345b31ace51b167c315f61
Mihamina Rakotovazaha <mihamina.rakotovazaha@digeiz.com>
2023-06-14 17:19:35 +0200

    feat: add marshmallow schema

---
+++ schemas.py
@@ -0,0 +1,22 @@
+"""API schema."""
+from marshmallow import Schema, fields
+
+
+class StoreSchema(Schema):
+    """Store schema."""
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+
+
+class ItemSchema(Schema):
+    """Item schema."""
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
+    price = fields.Float(required=True)
+    store_id = fields.Str(required=True)
+
+
+class ItemUpdateSchema(Schema):
+    """Item update schema."""
+    name = fields.Str()
+    price = fields.Float()
