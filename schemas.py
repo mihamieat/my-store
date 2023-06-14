e94ccdc9e48f0c3fe3338499a83104967c4e0db0
Mihamina Rakotovazaha <mihamina.rakotovazaha@digeiz.com>
2023-06-14 17:26:21 +0200

    style: lint code according to pre-commit hooks

--- schemas.py
+++ schemas.py
@@ -1,15 +1,18 @@
+# -*- coding: utf-8 -*-
 """API schema."""
 from marshmallow import Schema, fields
 
 
 class StoreSchema(Schema):
     """Store schema."""
+
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
 
 
 class ItemSchema(Schema):
     """Item schema."""
+
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
@@ -18,5 +21,6 @@ class ItemSchema(Schema):
 
 class ItemUpdateSchema(Schema):
     """Item update schema."""
+
     name = fields.Str()
     price = fields.Float()
