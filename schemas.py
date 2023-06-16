9d7f0a17a7bda2d2840f0580b9166db9dee186e2
Mihamina Rakotovazaha <mihamina.rakotovazaha@digeiz.com>
2023-06-16 11:10:40 +0200

    fix: correct id types in schmas

--- schemas.py
+++ schemas.py
@@ -6,14 +6,14 @@ from marshmallow import Schema, fields
 class StoreSchema(Schema):
     """Store schema."""
 
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
 
 
 class ItemSchema(Schema):
     """Item schema."""
 
-    id = fields.Str(dump_only=True)
+    id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
     store_id = fields.Str(required=True)
