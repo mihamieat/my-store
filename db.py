fca508753af45f387e9c8c47341ad3f77e9b4792
Mihamina Rakotovazaha <mihamina.rakotovazaha@digeiz.com>
2023-06-16 11:01:47 +0200

    feat: create SQLAlchemy models

--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """Database package."""
 
-stores = {}
-items = {
-    1: {"name": "Chair", "price": 17.99},
-    2: {"name": "Table", "price": 180.50},
-}
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy  # pylint: disable=C0103
