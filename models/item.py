739792a68fbec9777a719a87df2c917d1b73b1b7
Mihamina Rakotovazaha <mihamina.rakotovazaha@digeiz.com>
2023-06-16 16:27:54 +0200

    fix: correct table name method name

--- models/item.py
+++ models/item.py
@@ -7,7 +7,7 @@ from db import db
 class ItemModel(db.Model):
     """Item model."""
 
-    __table_name__ = "items"
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
--- models/store.py
+++ models/store.py
@@ -7,7 +7,7 @@ from db import db
 class StoreModel(db.Model):
     """Store model."""
 
-    __table_name__ = "stores"
+    __tablename__ = "stores"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
